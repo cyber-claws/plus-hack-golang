1578d0d9132b8b9f3b57546f6ee56afab44c2056
Ishmael Sibisi <ishmael@paystack.com>
2022-08-25 12:27:53 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cyber-claws/plus-hack-golang
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var randomKillTime = rand.Intn(300-60) + 60
+
+func ping(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Pong for now\n")
+}
+
+func main() {
+	http.HandleFunc("/ping", ping)
+	go func() {
+		fmt.Printf("A WARRIORS DEATH COME IN %d sec\n", randomKillTime)
+		time.Sleep(time.Duration(randomKillTime) * time.Second)
+		panic(fmt.Sprintf("SELF KILL AFTER %d sec\n", randomKillTime))
+	}()
+	http.ListenAndServe(":8080", nil)
+}
